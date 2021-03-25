f57d58223f177e6c283a0c58ceaaebf578b451d0
wanghang <wb-wx772876@alibaba-inc.com>
2021-03-25 11:52:10 +0800

    update

---
+++ src/main/java/com/wanghang/code/JVM/JVMDemo.java
@@ -0,0 +1,26 @@
+package com.wanghang.code.JVM;
+
+
+/**
+ *记录JVM相关的知识：
+ *a)GC算法的介绍：
+ * https://yuanyu.blog.csdn.net/article/details/87624961
+ *
+ *b)JVM:
+ * https://yuanyu.blog.csdn.net/article/details/87554692
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+public class JVMDemo {
+
+    public static void main(String[] args) {
+
+
+
+
+    }
+}
