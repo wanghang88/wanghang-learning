e044d126590dc26448e6d695a9d343de1bd72cac
wanghang <wb-wx772876@alibaba-inc.com>
2021-04-07 10:56:40 +0800

    update

--- src/main/java/com/wanghang/code/design/decorator/DecoratorTest2.java
+++ src/main/java/com/wanghang/code/design/decorator/DecoratorTest2.java
@@ -4,6 +4,11 @@ import com.wanghang.code.design.decorator.one.ConcreteComponent.ConcreteComponen
 import com.wanghang.code.design.decorator.one.ConcreteDecorator.ConcreteDecorator;
 import com.wanghang.code.design.decorator.one.component.Component;
 
+
+/**
+ *装饰器模式的具体运用：
+ * https://blog.csdn.net/csdn15698845876/article/details/81544562
+ */
 public class DecoratorTest2 {
     public static void main(String[] args) {
 
