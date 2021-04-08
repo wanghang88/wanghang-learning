b038f79ee88212b51ac1b5f51e91fc58861d83ae
wanghang <wb-wx772876@alibaba-inc.com>
2021-04-08 16:16:57 +0800

    update

--- src/main/java/com/wanghang/code/JDK/proxy/JDKProxyDemo.java
+++ src/main/java/com/wanghang/code/JDK/proxy/JDKProxyDemo.java
@@ -12,24 +12,26 @@ package com.wanghang.code.JDK.proxy;
  *b)Proxy：该类即为动态代理类
  *
  *2:如何实现JDK的动态代理：
- * 1)创建一个实现接口InvocationHandler的类，它必须实现invoke方法(本案例中的InvocationHandlerImpl,它又一个成员变需要代理的接口)
- * 2)创建被代理的类以及接口(本案例中的IUservice接口)
- * 3)通过Proxy的静态方法,newProxyInstance(ClassLoaderloader, Class[] interfaces, InvocationHandler h),
+ *2.1)创建一个实现接口InvocationHandler的类，它必须实现invoke方法(本案例中的InvocationHandlerImpl,它又一个成员变需要代理的接口)
+ *2.2)创建被代理的类以及接口(本案例中的IUservice接口)
+ *2.3)通过Proxy的静态方法,newProxyInstance(ClassLoaderloader, Class[] interfaces, InvocationHandler h),
  *              创建一个代理(本案例中创建的IUservice的代理对象)
- *4):通过代理调用方法:
+ *2.4):通过代理调用方法:
  *
  *
  *3:JDK动态代理的特点：
  *    运行阶段进行代理，通用性强，常见：JDK动态代理和CGLIB动态代理
  *
- *    JDK动态代理的性能不如CGLIB代理
+ *    JDK动态代理;
  *    JDK动态代理的被代理类必须是某个接口的实现，而CGLIB不用必须实现接口
  *    JDK动态代理是jdk自带代理，可直接使用；使用CGLIB代理需要额外引入CGLIB库，甚至要在配置文件中配置
  *
+ *    CGlib动态代理：
+ *    实现MethodInterceptor 重写intercept (methodProxy.invokeSuper(Object,args))
+ *    完全不受代理类必须实现接口类的限制,被代理的类不能申明为final,并且生成的代理类是原来类的子类
  *
- *
- *
- *
+ *4:关于spring如何使用动态代理：
+ *    4.1)可以任意的控制任意对象的执行过程，这个对象的执行过程可以由客户端灵活的指定,
  *
  *
  */
