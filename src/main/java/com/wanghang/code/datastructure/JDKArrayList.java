2c799dc3f24a47700520ba681e4e6f09097dc26c
wanghang <wb-wx772876@alibaba-inc.com>
2021-04-15 11:03:29 +0800

    update

--- src/main/java/com/wanghang/code/datastructure/JDKArrayList.java
+++ src/main/java/com/wanghang/code/datastructure/JDKArrayList.java
@@ -2,6 +2,8 @@ package com.wanghang.code.datastructure;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Random;
 
 
 public class JDKArrayList<E> implements List<E> {
@@ -103,6 +105,19 @@ public class JDKArrayList<E> implements List<E> {
 
 
     public static void main(String[] args) {
+
+        JDKArrayList jDKArrayList=new JDKArrayList();
+
+        //1:ArrayList测试方法：
+     //   jDKArrayList.arrayListTest();
+
+
+        //2:java.util.Collections,使用集合框架的工具类
+        jDKArrayList.arrayLisJavaUtilCollections();
+    }
+
+
+    private  void arrayListTest() {
         ArrayList<String> list=new ArrayList();
 
         Integer a=8;
@@ -113,14 +128,54 @@ public class JDKArrayList<E> implements List<E> {
         Object [] objectArr={0,1,2,3,4,5};
         //1:将原数组扩大至10位,并且包含之前的元素;这个方法Arrays.copyOf(objectArr,newLength)的核心方法
         objectArr= Arrays.copyOf(objectArr, 10);
-         for (int i = 0; i <objectArr.length ; i++) {
-             System.out.println("objectArr[i]:"+objectArr[i]);
+        for (int i = 0; i <objectArr.length ; i++) {
+            System.out.println("objectArr[i]:"+objectArr[i]);
+        }
+
+        //2:数组的移动(在数组中新加一个元素或者删除一个元素),特别是在指定位加元素或者删除元素
+        Object [] objectArr1={0,1,2,3,4,5};
+        System.arraycopy(objectArr1,2,objectArr1,2+1,6-2);
+        System.out.println("objectArr1:"+objectArr1);
+    }
+
+
+    //Collections.shuffle() 洗牌算法，其实就是将 List 集合中的元素进行打乱，一般可以用在抽奖、摇号、洗牌等各个场景中
+    private void arrayLisJavaUtilCollections() {
+        ArrayList<String> list=new ArrayList<String>();
+        list.add("1");
+        list.add("2");
+        list.add("3");
+        list.add("4");
+        list.add("5");
+        list.add("6");
+        list.add("7");
+        list.add("8");
+        list.add("9");
+        list.add("10");
+        for (int i = 0; i < 5; i++) {
+             Collections.shuffle(list);
+             System.out.println("list:"+list.toString());
          }
 
-         //2:数组的移动(在数组中新加一个元素或者删除一个元素),特别是在指定位加元素或者删除元素
-         Object [] objectArr1={0,1,2,3,4,5};
-         System.arraycopy(objectArr1,2,objectArr1,2+1,6-2);
-         System.out.println("objectArr1:"+objectArr1);
+        ArrayList<String> list2=new ArrayList<String>();
+        list2.add("1");
+        list2.add("2");
+        list2.add("3");
+        list2.add("4");
+        list2.add("5");
+        list2.add("6");
+        list2.add("7");
+        list2.add("8");
+        list2.add("9");
+        list2.add("10");
 
+        //可以把ArrayList或者Linkedlist，从指定的某个位置开始，进行正旋或者逆旋操作。有点像把集合理解成圆盘，把要的元素转到自己这，其他的元素顺序跟随
+        Collections.rotate(list2,1);
+        System.out.println("list2 rotate:"+list2.toString());
+
+        for (int i = 0; i < 3; i++) {
+            Collections.shuffle(list2,new Random(100));
+            System.out.println("list2:"+list.toString());
+        }
     }
 }
