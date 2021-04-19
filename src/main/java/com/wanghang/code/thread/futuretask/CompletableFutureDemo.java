1232f5a1a32245969f5379e46f9c969ec4e96357
wanghang <wb-wx772876@alibaba-inc.com>
2021-04-19 20:01:28 +0800

    update

--- src/main/java/com/wanghang/code/thread/futuretask/CompletableFutureDemo.java
+++ src/main/java/com/wanghang/code/thread/futuretask/CompletableFutureDemo.java
@@ -6,6 +6,25 @@ package com.wanghang.code.thread.futuretask;
  *
  * 1)参考博文:
  *         https://www.jianshu.com/p/6bac52527ca4
+ *
+ *
+ *
+ *
+ *  CompletableFuture[] cfs = idVersionMap.entrySet()
+ *                         .stream()
+ *                         .map(entry -> CompletableFuture.supplyAsync(()
+ *                                 -> findAppInstallVersion(entry.getKey(),entry.getValue().getVersionCode(),request,attributeData, appList), executorService)
+ *                                 .whenComplete((s, e) -> {
+ *                                     //System.out.println("分析appId:" + entry.getKey());
+ *                                 })
+ *                         ).toArray(CompletableFuture[]::new);
+ *                 CompletableFuture jobs = CompletableFuture.allOf(cfs);
+ *                 jobs.join();
+ *
+ *
+ *
+ *
+ *
  */
 public class CompletableFutureDemo {
 
