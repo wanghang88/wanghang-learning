2dedde4458b7a9b4565bbf46eb08b158496e53fe
wanghang <wb-wx772876@alibaba-inc.com>
2021-05-12 15:40:54 +0800

    update

--- src/main/java/com/wanghang/code/design/template/DataParserTemplateTest.java
+++ src/main/java/com/wanghang/code/design/template/DataParserTemplateTest.java
@@ -3,7 +3,14 @@ package com.wanghang.code.design.template;
 
 /**
  *模版方法模式：
+ *          抽象类中定义抽象方法的执行顺序，并将抽象方法设定为只有子类实现，但不设计独立访问的方法
  *          将一组流程定义在一个方法内，子类具有相同的功能在模版类中实现，不同的功能由子类自己实现
+ *
+ *2)模版方法模式参考实现:
+
+ https://bugstack.cn/itstack-demo-design/2020/07/07/%E9%87%8D%E5%AD%A6-Java-%E8%AE%BE%E8%AE%A1%E6%A8%A1%E5%BC%8F-%E5%AE%9E%E6%88%98%E6%A8%A1%E6%9D%BF%E6%A8%A1%E5%BC%8F.html
+
+ *
  *
  */
 public class DataParserTemplateTest {
