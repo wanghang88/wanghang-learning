4f94e2dd26e982d3a91ceaa969b16357b08d5e91
wanghang <wb-wx772876@alibaba-inc.com>
2021-06-29 11:08:34 +0800

    update

--- src/main/java/com/wanghang/code/MianShi/MianShiDemo1.java
+++ src/main/java/com/wanghang/code/MianShi/MianShiDemo1.java
@@ -2,7 +2,7 @@ package com.wanghang.code.MianShi;
 
 
 /**
-    java面试题目总结的博文:
+    1:java面试题目总结的博文:
     https://blog.csdn.net/wb_snail/article/details/108134550
 
     https://mp.weixin.qq.com/s?__biz=Mzg4NDMyNTIwMw==&mid=2247488123&idx=1&sn=9567232df10afbfede9688e4762c988c&chksm=cfb8bc47f8cf35512bced50381acb94713b51bb8691e895ba880a2b7863d7c882f4b54aec190&scene=21#wechat_redirect
@@ -14,17 +14,19 @@ package com.wanghang.code.MianShi;
     https://github.com/irwinai/JavaInterview/blob/main/%E6%9D%A5%E8%87%AA%E6%9C%8B%E5%8F%8B%E6%9C%80%E8%BF%91%E9%98%BF%E9%87%8C%E3%80%81%E8%85%BE%E8%AE%AF%E3%80%81%E7%BE%8E%E5%9B%A2%E7%AD%89P7%E5%B2%97%E4%BD%8D%E9%9D%A2%E8%AF%95%E9%A2%98.md
 
 
-
-
-    java面试相关的github：
+    2:java面试相关的github：
     //内容涵盖高并发、分布式、高可用、微服务、海量数据处理等领域知识
     https://github.com/aysaml/advanced-java
-   //就Hystrix介绍得比较不错,其他的不怎么行
-   https://gitee.com/ckl111/Java-Interview-Advanced#%E7%AC%AC%E4%B8%80%E5%AD%A3-%E5%88%86%E5%B8%83%E5%BC%8F
-
-
+    //就Hystrix介绍得比较不错,其他的不怎么行
+    https://gitee.com/ckl111/Java-Interview-Advanced#%E7%AC%AC%E4%B8%80%E5%AD%A3-%E5%88%86%E5%B8%83%E5%BC%8F
    java面试涵盖得比较全面(包括Java基础,线上问题排查，Spring,redis，dubbo,mysql,系统设计)
    https://github.com/AobingJava/JavaFamily
+
+
+
+   3:可以学习java的github
+   小傅哥(可以学习java基础的源码分析,Spring的源码分析，Netty的实战运用等)
+   https://github.com/fuzhengwei/CodeGuide#netty-4x-%E4%B8%93%E9%A2%98
 
 
  *
