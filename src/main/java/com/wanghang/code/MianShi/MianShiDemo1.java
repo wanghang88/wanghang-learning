4abb3bfc7e8b931a08aa988a7b9798a296980663
wanghang <wb-wx772876@alibaba-inc.com>
2021-06-30 11:23:56 +0800

    update

--- src/main/java/com/wanghang/code/MianShi/MianShiDemo1.java
+++ src/main/java/com/wanghang/code/MianShi/MianShiDemo1.java
@@ -2,31 +2,37 @@ package com.wanghang.code.MianShi;
 
 
 /**
-    1:java面试题目总结的博文:
+    1:java面试题目总结的博文(主要是面试题):
     https://blog.csdn.net/wb_snail/article/details/108134550
 
     https://mp.weixin.qq.com/s?__biz=Mzg4NDMyNTIwMw==&mid=2247488123&idx=1&sn=9567232df10afbfede9688e4762c988c&chksm=cfb8bc47f8cf35512bced50381acb94713b51bb8691e895ba880a2b7863d7c882f4b54aec190&scene=21#wechat_redirect
 
     https://blog.csdn.net/GitChat/article/details/78957557?utm_medium=distribute.pc_relevant.none-task-blog-2%7Edefault%7EBlogCommendFromBaidu%7Edefault-9.baidujs&depth_1-utm_source=distribute.pc_relevant.none-task-blog-2%7Edefault%7EBlogCommendFromBaidu%7Edefault-9.baidujs
 
-
     java基础总结得比较多:
     https://github.com/irwinai/JavaInterview/blob/main/%E6%9D%A5%E8%87%AA%E6%9C%8B%E5%8F%8B%E6%9C%80%E8%BF%91%E9%98%BF%E9%87%8C%E3%80%81%E8%85%BE%E8%AE%AF%E3%80%81%E7%BE%8E%E5%9B%A2%E7%AD%89P7%E5%B2%97%E4%BD%8D%E9%9D%A2%E8%AF%95%E9%A2%98.md
 
 
+    java进阶面试题,包含答案(synchronized,ReentrantLock,Java内存模型,volatile,volatile,线程池,Callable,Atomic原子等,AQS)
+    https://mp.weixin.qq.com/s/cdHfTTvMpH60SwG2bjTMBw
+
+
     2:java面试相关的github：
-    //内容涵盖高并发、分布式、高可用、微服务、海量数据处理等领域知识
+    2.1)内容涵盖高并发、分布式、高可用、微服务、海量数据处理等领域知识
     https://github.com/aysaml/advanced-java
-    //就Hystrix介绍得比较不错,其他的不怎么行
+    2.2)就Hystrix介绍得比较不错,其他的不怎么行
     https://gitee.com/ckl111/Java-Interview-Advanced#%E7%AC%AC%E4%B8%80%E5%AD%A3-%E5%88%86%E5%B8%83%E5%BC%8F
-   java面试涵盖得比较全面(包括Java基础,线上问题排查，Spring,redis，dubbo,mysql,系统设计)
-   https://github.com/AobingJava/JavaFamily
+    2.3)java面试涵盖得比较全面(包括Java基础,线上问题排查，Spring,redis，dubbo,mysql,系统设计)，敖丙三台子的搞的github
+    https://github.com/AobingJava/JavaFamily
+    2.4)JavaGuide的博客，主要有Java基础，特别是并发,JVM,Spring方面注重基础的面试总结
+    https://github.com/Snailclimb/JavaGuide
 
 
 
-   3:可以学习java的github
-   小傅哥(可以学习java基础的源码分析,Spring的源码分析，Netty的实战运用等)
-   https://github.com/fuzhengwei/CodeGuide#netty-4x-%E4%B8%93%E9%A2%98
+    3:可以学习java的github
+    3.1)小傅哥(可以学习java基础的源码分析,Spring的源码分析，Netty的实战运用等)
+    https://github.com/fuzhengwei/CodeGuide#netty-4x-%E4%B8%93%E9%A2%98
+
 
 
  *
