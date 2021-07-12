58c278c549cf1f2691aff22b229f24d2fa18e6b8
wanghang <wb-wx772876@alibaba-inc.com>
2021-07-12 18:32:59 +0800

    update

---
+++ src/main/java/com/wanghang/code/JDK/JDKDemo.java
@@ -0,0 +1,13 @@
+package com.wanghang.code.JDK;
+
+
+/**
+
+ JDK中的一些数据结构的讲解：
+ HashMap，栈，队列等(博主整理得不错)
+ https://blog.csdn.net/qq_26323323/category_9283744.html
+
+
+ */
+public class JDKDemo {
+}
--- src/main/java/com/wanghang/code/datastructure/DatastructureDemo.java
+++ src/main/java/com/wanghang/code/datastructure/DatastructureDemo.java
@@ -6,9 +6,8 @@ package com.wanghang.code.datastructure;
   https://blog.csdn.net/javazejian/article/details/53375004
 
 
-
-
-
+ 2：这一篇写的数据结构也还不错
+ https://blog.csdn.net/qq_26323323/category_7479084.html
 
  */
 public class DatastructureDemo {
