58bfcd157261062cfd609984d3851bda1fb7a61c
wanghang <wb-wx772876@alibaba-inc.com>
2021-07-20 11:07:40 +0800

    整理JVM的技术分类

--- src/main/java/com/wanghang/code/JVM/JVMDemo.java
+++ src/main/java/com/wanghang/code/JVM/JVMDemo.java
@@ -25,6 +25,9 @@ package com.wanghang.code.JVM;
 
 
 
+ 4:JVM的调优以及相关的问题排查:
+   JVM的常见的参数
+   https://mp.weixin.qq.com/s/XZFEgf1ZS7gNt7lku3TF4g
 
 
 
@@ -32,10 +35,14 @@ package com.wanghang.code.JVM;
 
 
 
- *
- *c)关于java虚拟机之直接内存(堆外内存)：
- *                1)不是虚拟机运行时数据区的一部分，也不是《Java虚拟机规范》中定义的内存区域，直接内存是Java堆外的，直接向系统申请的内存空间，
- *                    来源于NIO，通过存在堆中的DirectByteBuffer操作Native内存。
+
+
+
+
+
+  c)关于java虚拟机之直接内存(堆外内存)：
+                 1)不是虚拟机运行时数据区的一部分，也不是《Java虚拟机规范》中定义的内存区域，直接内存是Java堆外的，直接向系统申请的内存空间，
+                     来源于NIO，通过存在堆中的DirectByteBuffer操作Native内存。
  *                2)直接内存也可能导致OOM异常（OutOfMemoryError:Direct buffer memory)；
  *                3)由于直接内存是在Java堆外，因此它的大小不会直接受限于-Xmx指定的最大堆的大小，但是系统内存是有限的，Java堆和直接内存的总和依然受限于操作系统给出的最大内存,
  *                  直接内存的大小可以通过MaxDirectMemorySize设置,如果不指定，默认与堆的最大值-Xmx参数值一致
