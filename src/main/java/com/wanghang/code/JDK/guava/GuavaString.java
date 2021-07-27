1e95452ed8650312f735e7996961a8546c4d207f
wanghang <wb-wx772876@alibaba-inc.com>
2021-07-27 16:50:51 +0800

    update

---
+++ src/main/java/com/wanghang/code/JDK/guava/GuavaString.java
@@ -0,0 +1,17 @@
+package com.wanghang.code.JDK.guava;
+
+
+/**
+ *
+  Guava系列之强大的String工具类：
+ https://blog.csdn.net/pzjtian/article/details/106844856
+
+
+ *
+ */
+public class GuavaString {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/wanghang/code/JDK/guava/RateLimiterDemo.java
@@ -0,0 +1,30 @@
+package com.wanghang.code.JDK.guava;
+
+
+/**
+ *
+ * Guava系列之限流RateLimiter:
+   Guava中的限流使用的是令牌桶算法,只适用于单机的限流,RateLimiter提供了两种限流实现：
+
+  平滑突发限流(SmoothBursty)
+  平滑预热限流(SmoothWarmingUp)
+
+ 参考博文
+ https://blog.csdn.net/pzjtian/article/details/106863873
+
+
+
+
+
+ *
+ */
+
+public class RateLimiterDemo {
+
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
