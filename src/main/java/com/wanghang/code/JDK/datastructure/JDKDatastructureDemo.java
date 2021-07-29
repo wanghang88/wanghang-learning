36500cfb4c1052c67db76008e3b9734e019db1df
wanghang <wb-wx772876@alibaba-inc.com>
2021-07-29 15:13:33 +0800

    update

---
+++ src/main/java/com/wanghang/code/JDK/datastructure/JDKDatastructureDemo.java
@@ -0,0 +1,12 @@
+package com.wanghang.code.JDK.datastructure;
+
+
+/**
+  JDK中的数据结构的总结:
+  1:参考恐龙弟旺的博文(还可以)
+  https://blog.csdn.net/qq_26323323/category_9283744.html
+
+
+ */
+public class JDKDatastructureDemo {
+}
--- src/main/java/com/wanghang/code/datastructure/DatastructureDemo.java
+++ src/main/java/com/wanghang/code/datastructure/DatastructureDemo.java
@@ -3,15 +3,17 @@ package com.wanghang.code.datastructure;
 
 /**
  *1:数据结构之参考博文：
-  https://blog.csdn.net/javazejian/article/details/53375004
+    https://blog.csdn.net/javazejian/article/details/53375004
 
 
- 2：这一篇写的数据结构也还不错
- https://blog.csdn.net/qq_26323323/category_7479084.html
+    2:这一篇写的数据结构也还不错
+    CSDN(恐龙弟旺仔)的博文
+    https://blog.csdn.net/qq_26323323/category_7479084.html
 
 
- 3:这个博主的数据结构也还可以:
- https://blog.csdn.net/qq_41907991/category_9292783.html
+    3:这个博主的数据结构也还可以:
+    CSDN(程序员DMZ)的博文
+    https://blog.csdn.net/qq_41907991/category_9292783.html
 
  */
 public class DatastructureDemo {
