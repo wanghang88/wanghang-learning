3d6460ac9dd65ef620c854fc3076d0f6955759b4
wanghang <wb-wx772876@alibaba-inc.com>
2021-08-10 10:20:16 +0800

    update

---
+++ src/main/java/com/wanghang/code/algorithm/RedEnvelopeDemo.java
@@ -0,0 +1,26 @@
+package com.wanghang.code.algorithm;
+
+
+/**
+
+ java实现抢红包算法：
+ 参考博文：
+ https://mp.weixin.qq.com/s/ccZLhkWCmpNnT_uM-IbQog
+
+
+ 方案一:二倍均值法(随机抢)
+
+
+
+ 方案二:线段切割法(拼手速)
+
+
+
+
+
+
+
+
+ */
+public class RedEnvelopeDemo {
+}
