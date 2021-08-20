74b02a6d11f2f387b52cd191582a79ffdcf3c572
wanghang <wb-wx772876@alibaba-inc.com>
2021-08-20 19:41:45 +0800

    面试相关的问题整理

--- src/main/java/com/wanghang/code/MianShi/MianShiDemo1.java
+++ src/main/java/com/wanghang/code/MianShi/MianShiDemo1.java
@@ -1,17 +1,19 @@
 package com.wanghang.code.MianShi;
 
 /**
-    1:java面试题目总结的博文(主要是面试题):
-    https://blog.csdn.net/wb_snail/article/details/108134550
-
+    1:面试题
+    面试题收集(有答案)
     https://mp.weixin.qq.com/s?__biz=Mzg4NDMyNTIwMw==&mid=2247488123&idx=1&sn=9567232df10afbfede9688e4762c988c&chksm=cfb8bc47f8cf35512bced50381acb94713b51bb8691e895ba880a2b7863d7c882f4b54aec190&scene=21#wechat_redirect
-
     https://blog.csdn.net/GitChat/article/details/78957557?utm_medium=distribute.pc_relevant.none-task-blog-2%7Edefault%7EBlogCommendFromBaidu%7Edefault-9.baidujs&depth_1-utm_source=distribute.pc_relevant.none-task-blog-2%7Edefault%7EBlogCommendFromBaidu%7Edefault-9.baidujs
 
-    java基础总结得比较多:
+    面试题目收集(无答案)
     https://github.com/irwinai/JavaInterview/blob/main/%E6%9D%A5%E8%87%AA%E6%9C%8B%E5%8F%8B%E6%9C%80%E8%BF%91%E9%98%BF%E9%87%8C%E3%80%81%E8%85%BE%E8%AE%AF%E3%80%81%E7%BE%8E%E5%9B%A2%E7%AD%89P7%E5%B2%97%E4%BD%8D%E9%9D%A2%E8%AF%95%E9%A2%98.md
+    https://susan.net.cn/post/9/9004/
+    https://blog.csdn.net/wb_snail/article/details/108134550
 
-    java进阶面试题,包含答案(synchronized,ReentrantLock,Java内存模型,volatile,volatile,线程池,Callable,Atomic原子等,AQS)
+
+
+ java进阶面试题,包含答案(synchronized,ReentrantLock,Java内存模型,volatile,volatile,线程池,Callable,Atomic原子等,AQS)
     https://mp.weixin.qq.com/s/cdHfTTvMpH60SwG2bjTMBw
 
     java基础juc中 LockSupport & AQS & 各种锁 & 分布式锁的分析，特别是对锁的分析，互斥锁，aqs，分布式锁等
@@ -92,6 +94,9 @@ package com.wanghang.code.MianShi;
 
     面试HR可能会问到的问题
     https://blog.csdn.net/chuanchengdabing/article/details/118769749
+
+
+
 
 
 
