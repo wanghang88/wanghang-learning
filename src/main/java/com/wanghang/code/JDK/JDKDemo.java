67f90e2ec289bb99ceac8e97449709e4b68ac332
wanghang <wb-wx772876@alibaba-inc.com>
2021-08-26 11:55:17 +0800

    update

--- src/main/java/com/wanghang/code/JDK/JDKDemo.java
+++ src/main/java/com/wanghang/code/JDK/JDKDemo.java
@@ -10,19 +10,8 @@ package com.wanghang.code.JDK;
  这一篇也可以,还包括对Hash表原理的介绍(也是基于java8的)
  https://blog.csdn.net/qq_26323323/article/details/86219905
 
- HashMap1.7源码剖析
- https://blog.csdn.net/yxh13521338301/article/details/105535513
-
- 头插法会导致hashmap7扩容死循环而尾插法却不会
- https://blog.csdn.net/yxh13521338301/article/details/105629318
 
 
- JDK1.8ConcurrentHashMap源码剖析(最后还有总结)
- https://blog.csdn.net/yxh13521338301/article/details/105598561
-
-
- 源码分析ArrayList、LinkedList和Vector（有详细的分析,并且最后还有总结）
- https://blog.csdn.net/yxh13521338301/article/details/105511053
 
  */
 public class JDKDemo {
--- src/main/java/com/wanghang/code/design/DesignDemo.java
+++ src/main/java/com/wanghang/code/design/DesignDemo.java
@@ -3,16 +3,26 @@ package com.wanghang.code.design;
 
 /**
 
-
+  1：
   常用的设计模式:(桥接模式、组合模式、装饰模式、门面模式、代理模式、责任链模式)
   https://mp.weixin.qq.com/s/P-W-FwqtXLkDjjKJYi9HjQ
-
-
 
   常见的设计模式,公众号:码哥字节,(这个作者还是不错的)
   https://mp.weixin.qq.com/mp/appmsgalbum?__biz=MzkzMDI1NjcyOQ==&action=getalbum&album_id=1920649346379759617&scene=173&from_msgid=2247487626&from_itemidx=1&count=3&nolastread=1#wechat_redirect
   对应的github:
   https://github.com/MageByte-Zero/zero-design-patterns
+
+
+ 2:设计模式在实际的业务中的使用;
+
+  一种思路：策略模式 + 反射工厂，很好的实现了开闭原则
+ https://mp.weixin.qq.com/s/rxiHqUh080U5fGDhn0Q_UQ
+
+
+
+
+
+
 
 
 
