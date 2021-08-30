adab1a7d4a3bc9fbb17eddedd7e8b6f3df15a3bb
wanghang <wb-wx772876@alibaba-inc.com>
2021-08-30 10:42:55 +0800

    update

---
+++ src/main/java/com/wanghang/code/MianShi/MianshiDemo2.java
@@ -0,0 +1,54 @@
+package com.wanghang.code.MianShi;
+
+
+/**
+  收集网友面试题总结(都会总结):
+
+  网友1：(红黑树,线程池,hashmap1.7和1.8,threadlocal的使用与原理，redis持久化，分库分表)
+        JAVA中哪些地方用到了红黑树？
+        hashmap中的红黑树如果用一种非树状结构的线性结构替换，要用什么比较好？
+        tcpip是怎么保证连接的可靠性的？
+        redis分布式锁使用场景和原理？
+        做过的项目最高的并发量有多少？会不会有一些并发方面的问题？
+        线程池用过吗，有哪些应用场景，底层原理了解吗？
+        如何让private的方法的事物标记生效？
+        用JAVA的api如何实现一个压测工具？
+
+        说一下hashmap1.7和1.8的数据结构
+        redis两种持久化模式说一下？
+        mysql的binlog文件和redo文件分别有什么作用？为什么要有redo文件？
+        分布式锁用过吗，使用场景和原理？
+        分库分表做过吗？怎么处理的？
+        redis在做持久化的时候，主线程可能会比较卡，如何优化这个过程？
+        reactor和preactor有什么区别？
+        select. poll. epoll有什么区别，为什么要做这个优化？
+        线上jvm进程cpu负载不高，但是卡顿，如何解决？
+        threadlocal的使用与原理？
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ */
+public class MianshiDemo2 {
+}
